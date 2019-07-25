e8bcaf17a1cea7c3f4da34d3bb73511168d46a8a
Laksh Bhambhani <lakshbh@gmail.com>
2019-07-25 12:10:07 -0700

    Gyroscope sensing will now make Swiffee turn when user turns.
    Part 2 of SwiffeeVR Completed. Testing stiill needs to be done tho

--- Swiffee VR/Swiffee VR/ViewController.swift
+++ Swiffee VR/Swiffee VR/ViewController.swift
@@ -15,18 +15,28 @@ import CoreMotion
 class ViewController: UIViewController, WKUIDelegate {
     
    var manager = CMMotionManager()
+    var loadView: WKWebView!
+    
+    let ip = "192.168.0.33"
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let webConfiguration = WKWebViewConfiguration()
+        loadView = WKWebView(frame: .init(x: 1000, y: 1000, width: 0, height: 0), configuration: webConfiguration)
+        loadView.uiDelegate = self
+        
         let webView = WKWebView(frame: CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: self.view.frame.size.height))
         let localfilePath = Bundle.main.url(forResource: "home", withExtension: "html");
         let myRequest = NSURLRequest(url: localfilePath!);
         webView.load(myRequest as URLRequest);
         self.view.addSubview(webView)
+        
+        gyroscope()
     }
     
     func gyroscope(){
-        manager.gyroUpdateInterval = 0.1
+        manager.gyroUpdateInterval = 1.0 / 25.0
         manager.startGyroUpdates(to: OperationQueue.current!){ (data, error) in
         print(data as Any)
             if let trueData = data{
@@ -34,11 +44,88 @@ class ViewController: UIViewController, WKUIDelegate {
                 let x =  trueData.rotationRate.x
                 let y =  trueData.rotationRate.y
                 let z =  trueData.rotationRate.z
-                print(x + y + z)
+                
+                var lastMotion = ""
+                
+                if y < 0.5{
+                    if lastMotion != "bendBack" && lastMotion != "bow"{
+                        self.bendBackAction()
+                        lastMotion = "bendBack"
+                    }
+                    else{
+                        self.homeAction()
+                        lastMotion =  "homePos"
+                    }
+                }
+                if y > -0.5 {
+                    if lastMotion != "bow" && lastMotion != "bendBack"{
+                        self.bowAction()
+                        lastMotion = "bow"
+                    }
+                    else{
+                        self.homeAction()
+                        lastMotion = "homePos"
+                    }
+                }
+                
+                if x > 0.5{
+                    if lastMotion != "right"{
+                        self.rightAction()
+                        lastMotion = "right"
+                    }
+                }
+                if y < -0.5 {
+                    if lastMotion != "left"{
+                        self.leftAction()
+                        lastMotion = "left"
+                    }
+                }
+//                print("x: \(Double(x).rounded(toPlaces :3))")
+//                print("y: \(Double(y).rounded(toPlaces :3))")
+//                print("z: \(Double(z).rounded(toPlaces :3))")
             }
         }
     }
     
+    func leftAction() {
+        print("Left Clicked")
+        let url = URL(string: "http://" + ip + "/turnLeft");
+        let request = URLRequest(url: url!);
+        loadView.load(request);
+        print(url)
+    }
+    
+    func rightAction() {
+        print("Right Clicked")
+        let url = URL(string: "http://" + ip + "/turnRight");
+        let request = URLRequest(url: url!);
+        loadView.load(request);
+        print(url)
+    }
+    
+    func bowAction() {
+        print("Bow Clicked")
+        let url = URL(string: "http://" + ip + "/bow");
+        let request = URLRequest(url: url!);
+        loadView.load(request);
+        print(url)
+    }
+    
+    func bendBackAction() {
+        print("Bend Back Clicked")
+        let url = URL(string: "http://" + ip + "/bendBack");
+        let request = URLRequest(url: url!);
+        loadView.load(request);
+        print(url)
+    }
+    
+    func homeAction() {
+            print("Home Clicked")
+            let url = URL(string: "http://" + ip + "/homePos");
+            let request = URLRequest(url: url!);
+            loadView.load(request);
+            print(url)
+        }
     
 
 }
